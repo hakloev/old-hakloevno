e7f8b2f5e84b7cfa9075e9d9d88098a1edc7bdac
Håkon Ødegård Løvdal <hakloev@gmail.com>
2014-06-08 01:40:26 +0200

    url fix

--- beertasting/urls.py
+++ beertasting/urls.py
@@ -7,8 +7,8 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^$', index),
-    url(r'event(?P<id>[0-9]+)/$', event_by_id, name="event_by_id"),
-    url(r'event(?P<eid>[0-9]+)/ratebeer(?P<bid>[0-9]+)/$', beer_rating, name="beer_rating"),
+    url(r'event-(?P<id>[0-9]+)/$', event_by_id, name="event_by_id"),
+    url(r'event-(?P<eid>[0-9]+)/rate-beer-(?P<bid>[0-9]+)/$', beer_rating, name="beer_rating"),
     #url(r'^newpost/$', newpost),
     #url(r'^posts/(?P<slug>[-a-zA-Z0-9]+)/$', post_by_slug, name='post_by_slug'),
 )
