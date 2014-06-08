be17ef711e90b166f2c9be8e37efdf998d148348
Håkon Ødegård Løvdal <hakloev@gmail.com>
2014-06-08 15:07:13 +0200

    beer rating

--- beertasting/views.py
+++ beertasting/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from models import BeerRating, TastingEvent
+from django.http import HttpResponseRedirect
 import datetime
 
 # Create your views here.
@@ -21,14 +22,21 @@ def event_by_id(request, id):
     context['request'] = request
     event = TastingEvent.objects.get(id=id)
     context['event'] = event
-    if event.finished:
-        ratings = BeerRating.objects.filter(event=id)
-        context['ratings'] = ratings
-    else:
-        context['beers'] = event.beers
+    context['beers'] = event.beers
     return render(request, u'beertasting/event.html', context)
-
 
 def beer_rating(request, eid, bid):
     context = {}
+    context['request'] = request
+    # ta imot post request, parse og putt i db 
+    if request.method == 'POST':
+        return HttpResponseRedirect(request.path)
+    event = TastingEvent.objects.get(id=eid)
+    context['event'] = event
+    try: 
+        rating = BeerRating.objects.get(event=eid, beer=bid, user=request.user.id)
+        context['rating'] = rating
+    except:
+        pass
+     
     return render(request, u'beertasting/ratebeer.html', context)
