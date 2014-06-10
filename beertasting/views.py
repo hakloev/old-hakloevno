b3696971a4642070920b5881cf082889c86666f3
Håkon Ødegård Løvdal <hakloev@gmail.com>
2014-06-11 01:29:58 +0200

    clean up

--- beertasting/views.py
+++ beertasting/views.py
@@ -10,59 +10,44 @@ import datetime
 # Create your views here.
 
 def index(request):
-    if not request.user.is_authenticated():
-        return HttpResponseRedirect('/accounts/login/?next=/beertasting/')
-    context = {}
-    context['request'] = request
+    latestevents = TastingEvent.objects.filter(finished=False).order_by('id')
+    doneevents = TastingEvent.objects.filter(finished=True).order_by('-id')
+    
     breadcrumbs = (
-            ('Arrangementer', '/beertasting/'),
-            )
-    context['breadcrumbs'] = breadcrumbs
-    try:
-        latestevents = TastingEvent.objects.filter(finished=False).order_by('id')
-        context['latestevents'] = latestevents
-        events = TastingEvent.objects.filter(finished=True).order_by('-id')
-        context['events'] = events
-    except:
-        pass
-    return render(request, u'beertasting/index.html', context)
+        ('Arrangementer', '/beertasting/'),
+    )
+
+    return render(request, u'beertasting/index.html', {
+        'request': request, 
+        'breadcrumbs': breadcrumbs,
+        'latestevents': latestevents,
+        'doneevents': doneevents}
+    )
 
 def event_by_id(request, id):
     if not request.user.is_authenticated():
-        return HttpResponseRedirect('/accounts/login/?next=/beertasting/event%s' % (id))
-    context = {}
-    context['request'] = request
+        return HttpResponseRedirect('/') #TODO: fix redirect to unauthorized
     event = TastingEvent.objects.get(id=id)
-    context['event'] = event
+    # Ratings does only show when event is finished. Check for that in view for optimalization?
+    ratings = BeerRating.objects.filter(event=id, user_id=request.user.id)
+    
     breadcrumbs = (
             ('Arrangementer', '/beertasting/'),
             (event.name, reverse('event_by_id', args=[event.id])),
-            )
-    context['breadcrumbs'] = breadcrumbs
+    )
 
-    # redirect til stats if finished?
-    if event.finished:
-        ratings = BeerRating.objects.filter(event=id, user_id=request.user.id)
-        context['ratings'] = ratings
-    context['beers'] = event.beers
-    return render(request, u'beertasting/event.html', context)
+    return render(request, u'beertasting/event.html', {
+        'request':request,
+        'event': event,
+        'ratings': ratings,
+        'breadcrumbs':breadcrumbs}
+    )
 
 def beer_rating(request, eid, bid):
     if not request.user.is_authenticated():
-        return HttpResponseRedirect('/accounts/login/?next=/beertasting/event%s/beer%s' % (eid, bid))
-    context = {}
-    context['request'] = request
+        return HttpResponseRedirect('/') #TODO: fix redirect to unauthorized
     event = TastingEvent.objects.get(id=eid)
-    context['event'] = event
-    context['beerid'] = bid
-    context['beers'] = event.beers
-    breadcrumbs = (
-            ('Arrangementer', '/beertasting/'),
-            (event.name, reverse('event_by_id', args=[event.id])),
-            (u'Ølnummer %s' % (bid), reverse('beer_rating', args=[event.id, bid])),
-            )
-    context['breadcrumbs'] = breadcrumbs
-
+    
     if request.method == "POST":
         rating = request.POST['ratingvalue']
         comment = request.POST['comment']
@@ -75,14 +60,27 @@ def beer_rating(request, eid, bid):
         except:
             new_r = BeerRating(user=request.user, beer_id=bid, event_id=eid, rating=rating, comment=comment )
             new_r.save()
-        messages.success(request, 'Dine stemme ble registrert!')
+        messages.success(request, u'Dine stemme for øl %s ble registrert!' % (bid))
         return HttpResponseRedirect(request.path)
+    rating = None
     try: 
         rating = BeerRating.objects.get(event=eid, beer=bid, user=request.user.id)
-        context['rating'] = rating
     except:
         pass
-    return render(request, u'beertasting/ratebeer.html', context)
+    
+    breadcrumbs = (
+            ('Arrangementer', '/beertasting/'),
+            (event.name, reverse('event_by_id', args=[event.id])),
+            (u'Øl %s' % (bid), reverse('beer_rating', args=[event.id, bid])),
+    )
+
+    return render(request, u'beertasting/ratebeer.html', {
+        'request': request,
+        'event': event,
+        'beerid': bid,
+        'rating': rating,
+        'breadcrumbs': breadcrumbs}
+    )
 
 def stats(request, eid):
     context = {}
