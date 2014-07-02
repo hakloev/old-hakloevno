d4517d55807c8c669e70a841e41a42467793f763
Håkon Ødegård Løvdal <hakloev@gmail.com>
2014-07-02 20:58:09 +0200

    work on cv

--- loevdalnet/static/js/cv.js
+++ loevdalnet/static/js/cv.js
@@ -1,16 +1,6 @@
-function loadPage(href) {
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.open("GET", href, false);
-    xmlhttp.send();
-    return xmlhttp.responseText;
-}
-
-function replacePage() {
-    document.querySelector('.active').className = "";
-    document.querySelector('#cv').className = "active";
-    var stateObject = {};
-    var title = "CV";
-    var newUrl = "/cv/";
-    history.pushState(stateObject, title, newUrl);
-    document.querySelector('.maincontainer').innerHTML = loadPage('/static/public/cv.html');
+function replaceNavbar() {
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".container { width: 900px }";
+    document.body.appendChild(css);
 }
