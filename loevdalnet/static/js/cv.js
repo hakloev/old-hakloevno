5f4051ed31be4425d87f5090a9ae7ad093bfc32f
Håkon Ødegård Løvdal <hakloev@gmail.com>
2014-07-08 20:18:04 +0200

    clean up

--- loevdalnet/static/js/cv.js
+++
@@ -1,6 +0,0 @@
-function replaceNavbar() {
-    var css = document.createElement("style");
-    css.type = "text/css";
-    css.innerHTML = ".container { width: 900px }";
-    document.body.appendChild(css);
-}
