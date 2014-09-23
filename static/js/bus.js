52fd2e349d18b67ddc6b2b5c5a06da5d5c59c0c8
Håkon Ødegård Løvdal <hakloev@gmail.com>
2014-09-23 21:20:05 +0200

    fixed bus js

--- static/js/bus.js
+++ static/js/bus.js
@@ -1,63 +1,96 @@
-var busInfoUrl = "/bus";
-
-var xmlReq = function(url, type, callback) {
-    var xhr = new XMLHttpRequest();
-    xhr.onload = function () {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            console.log('Callback in xhr');
-            callback(this.responseText);
+var Bus = {
+    busInfoUrl: "/bus/",
+    parsed: false,
+    runnedBefore: false,
+    getBusInfo: function() {
+        var self = this; // Needed to access parseInfo in callback function
+        if (this.runnedBefore) {
+            this.resetInfo();
+        }
+        this.xmlReq(this.busInfoUrl, "GET", 
+            function(responseText) {
+                var json = JSON.parse(responseText);
+                self.parseInfo(json.berg);
+                self.parsed = true;
+                self.parseInfo(json.ila);
+                document.getElementById("busloading").innerHTML = "";
+                document.getElementById("bergname").innerHTML = "Østre Berg:";
+                document.getElementById("ilaname").innerHTML = "Ila:";
+                self.runnedBefore = true;
+            }
+        );
+    },
+    resetInfo: function() {
+        this.parsed = false;
+        this.runnedBefore = false;
+        document.getElementById("busberglist").innerHTML = "";
+        document.getElementById("busilalist").innerHTML = "";
+    },
+    xmlReq:  function(url, type, callback) {
+        var xhr = new XMLHttpRequest();
+        xhr.onload = function () {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                console.log('INFO: Callback in XMLHttpRequest');
+                callback(this.responseText);
+            } else {
+                console.log('ERROR: XMLHttpRequest failed, nothing to display in DOM');
+            }
+        }
+        xhr.open(type, url, true);
+        xhr.send();
+    }, 
+    parseInfo: function(place) {
+        console.log("INFO: parseInfo called");
+        var departureList = [];
+        for (var i = 0; i < place.next.length; i++) {
+            if (place.next[i].l == "5") {
+                departureList.push(place.next[i]);
+            }
+            if (departureList.length == 3) {
+                break;
+            }
+        }
+        this.printInfo(departureList);
+    }, 
+    printInfo: function(list) {
+        console.log("INFO: printInfo called");
+        var place = "busberglist";
+        if (this.parsed) {
+            place = "busilalist";
+        }
+        for (var i = 0; i < list.length; i++) {
+            console.log(list[i]);
+            var ul = document.getElementById(place);
+            var li = document.createElement("li");
+            var p = document.createElement("p");
+            var small = document.createElement("small");
+            var text1 = document.createTextNode(" - " + this.calcTime(list[i].t) + " min ");
+            small.appendChild(text1);
+            var text2 = document.createTextNode(list[i].t.substring(11, 16));
+            p.appendChild(text2);
+            p.appendChild(small);
+            li.appendChild(p);
+            ul.appendChild(li);
+        } 
+    },
+    calcTime: function(time) {
+        console.log("INFO: calcTime called");
+        var d = time.match(/^(\d{2}).(\d{2}).(\d{4}) (\d{2}):(\d{2})$/);
+        var today = new Date();
+        var departure = new Date(d[3], d[2] - 1, d[1], d[4], d[5]);
+        var diff = Math.floor((departure.getTime() - today.getTime()) / (1000 * 60));
+        if (diff <= -1 && diff <= 0) {
+            return "ca nå";
         } else {
-            console.log('Error with xhr');
-        }
+            return "ca " + diff;
+        }   
     }
-    xhr.open(type, url, true);
-    xhr.send();
-}
-
-function parseInfo(place) {
-    var bergInfo = place.berg;
-    var ilaInfo = place.ila;
-    var stopList = [];
-    for (var i = 0; i < bergInfo.next.length; i++) {
-        if (bergInfo.next[i].l == "5") {
-            stopList.push(bergInfo.next[i]);
-        }
-        if (stopList.length == 4) {
-            break;
-        }
-    }
-    printInfo(stopList, "busberglist");
-    stopList = [];
-    for (var i = 0; i < ilaInfo.next.length; i++) {
-        if (ilaInfo.next[i].l == "5") {
-            stopList.push(ilaInfo.next[i]);
-        }
-         if (stopList.length == 4) {
-            break;
-        }
-    } 
-    printInfo(stopList, "busilalist");
-}
-
-function printInfo(times, type) {
-    for (var i = 0; i <times.length; i++) {
-        console.log(times[i].t.substring(11,16) + ' til ' + times[i].d)
-        var ul = document.getElementById(type);
-        var li = document.createElement("li");
-        li.appendChild(document.createTextNode(times[i].t.substring(11, 16) + ' (' + times[i].rt + ')' ));
-        ul.appendChild(li);
-    }
-}
-
-function getBusTimes() {
-    xmlReq(busInfoUrl, 'GET', 
-        function(responseText) {
-            var stopTimes = JSON.parse(responseText);
-            parseInfo(stopTimes);
-        }
-    );
-}
+  
+};
 
 $(document).ready(function () {
-    getBusTimes();
+    Bus.getBusInfo();
+    setInterval( function() {
+            Bus.getBusInfo();
+    }, 60000);
 });
