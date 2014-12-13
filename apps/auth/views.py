47e293f795e92eb9dd12bd68e76c723e30285b77
Håkon Ødegård Løvdal <hakloev@gmail.com>
2014-12-13 21:01:01 +0100

    this is the motherfucking login form

---
+++ apps/auth/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
+# Create your views here.
+def login_view(request):
+    redirect_url = request.REQUEST.get('next', '')
+    if request.POST:
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                messages.success(request, 'You successfully logged in as %s' % (user.username))            
+                if redirect_url:
+                    return HttpResponseRedirect(redirect_url)
+                return HttpResponseRedirect('/') 
+            else:
+                pass
+                #return "disabled account"
+        else:
+            pass
+            #return invalid account
+    return render(request, u'auth/login.html', {
+        'request': request
+    })
+
+def logout_view(request):
+    logout(request)
+    messages.success(request, 'You successfully logged out!')
+    return HttpResponseRedirect('/')
+    #redirect to login success page
