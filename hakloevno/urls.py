dac3e7babfd6f12164062d871cc4dcf4d2bfbbfd
Håkon Ødegård Løvdal <hakloev@gmail.com>
2014-12-16 22:08:28 +0100

    dict stats

--- hakloevno/urls.py
+++ hakloevno/urls.py
@@ -14,6 +14,6 @@ urlpatterns = patterns('',
     url(r'^auth/', include('apps.auth.urls', namespace="auth")),
     url(r'^bustimes/$', bus_times),
     url(r'^busstops/', bus_stops),
-    url(r'^blog/', include('apps.blog.urls', namespace='blog')), # This can be deleted, but leave it temporarily 'cause it is easy
+    url(r'^blog/', include('apps.blog.urls', namespace='blog')),
     url(r'^tasting/', include('apps.beertasting.urls', namespace='tasting')),
     ) 
